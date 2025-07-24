1f9bd7f4de536e585ee2ed8ad6da1feb97e30784
inischal <nspnischal@gmail.com>
2025-07-25 00:48:31 +0545

    implement PostDetail page to show full blog post
    handle loading state when post is being fetched

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PostDetail from "./pages/PostDetail";
 import Home from "./pages/Home";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="p-3 max-w-4xl mx-auto">
-      <h1 className="text-2xl font-bold text-center">Blog</h1>
+      <h1 className="text-2xl font-bold text-center">Blog Website</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts/:id" element={<PostDetail />} />
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const BlogCard = ({ post }) => {
   return (
     <div className="border p-3 mb-3">
-      <h2 className="text-xl">{post.title}</h2>
+      <h2 className="text-xl text-zinc-900 font-bold">{post.title}</h2>
       <p>{post.body}</p>
-      <Link to={`/posts/${post.id}`} className="text-blue-400">
+      <Link to={`/posts/${post.id}`} className="text-white">
         Read More
       </Link>
     </div>
